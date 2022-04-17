c037d30231ca8774a23c08a9f12b3143efb9d1c3
Emre GEMİCİ <gmcc.emree@gmail.com>
2022-04-17 08:32:37 +0300

    Add project files.

---
+++ AspNetMVC_Template/Controllers/UrunController.cs
@@ -0,0 +1,36 @@
+﻿using AspNetMVC_Template.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVC_Template.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        NorthwindEntities ctx = new NorthwindEntities();
+        public ActionResult Index()
+        {
+            List<Urunler> urunler = ctx.Urunlers.ToList();
+            return View(urunler);
+        }
+
+        [HttpGet]
+        public ActionResult UrunEkle()
+        {
+            ViewBag.Kategoriler = ctx.Kategorilers.ToList();
+            ViewBag.Tedarikciler = ctx.Tedarikcilers.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UrunEkle(Urunler urun)
+        {
+            ctx.Urunlers.Add(urun);
+            ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
