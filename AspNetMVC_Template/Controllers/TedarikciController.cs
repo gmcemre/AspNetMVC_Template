409dbac36617d659fd76357b7287e8090ee8ac1f
Emre GEMİCİ <gmcc.emree@gmail.com>
2022-04-18 05:54:13 +0300

    modal added

---
+++ AspNetMVC_Template/Controllers/TedarikciController.cs
@@ -0,0 +1,36 @@
+﻿using AspNetMVC_Template.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVC_Template.Controllers
+{
+    public class TedarikciController : Controller
+    {
+        // GET: Tedarikci
+        NorthwindEntities ctx = new NorthwindEntities();
+        public ActionResult Index()
+        {
+            List<Tedarikciler> ted = ctx.Tedarikcilers.ToList();
+            return View(ted);
+        }
+
+        [HttpPost]
+        public string Sil(int id)
+        {
+            Tedarikciler t = ctx.Tedarikcilers.FirstOrDefault(x => x.TedarikciID == id);
+            ctx.Tedarikcilers.Remove(t);
+            try
+            {
+                ctx.SaveChanges();
+                return "Başarılı";
+            }
+            catch (Exception)
+            {
+                return "Hata";
+            }
+        }
+    }
+}
