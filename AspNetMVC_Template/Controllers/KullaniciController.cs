af9a9afbd8edba4a5d90d9f90bfd66b8155d8714
Emre GEMİCİ <gmcc.emree@gmail.com>
2022-05-05 03:22:13 +0300

    role managment

--- AspNetMVC_Template/Controllers/KullaniciController.cs
+++ AspNetMVC_Template/Controllers/KullaniciController.cs
@@ -1,4 +1,5 @@
-﻿using AspNetMVC_Template.Models;
+﻿using AspNetMVC_Template.App_Classes;
+using AspNetMVC_Template.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,95 @@ namespace AspNetMVC_Template.Controllers
         {
             MembershipUserCollection users = Membership.GetAllUsers();
             return View(users);
+        }
+
+        [HttpGet]
+        public ActionResult KullaniciEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KullaniciEkle(Kullanici k)
+        {
+            MembershipCreateStatus durum;
+            Membership.CreateUser(k.KullaniciAdi, k.Parola, k.Email, k.GizliSoru, k.GizliCevap, true, out durum);
+            string message = "";
+            switch (durum)
+            {
+                case MembershipCreateStatus.Success:
+
+                    break;
+                case MembershipCreateStatus.InvalidUserName:
+                    message += " Geçersiz Kullanıcı Adı ";
+                    break;
+                case MembershipCreateStatus.InvalidPassword:
+                    message += " Geçersiz Parola ";
+                    break;
+                case MembershipCreateStatus.InvalidQuestion:
+                    message += " Geçersiz Gizli Soru ";
+                    break;
+                case MembershipCreateStatus.InvalidAnswer:
+                    message += " Geçersiz Gizli Cevap ";
+                    break;
+                case MembershipCreateStatus.InvalidEmail:
+                    message += " Geçersiz Mail Adresi ";
+                    break;
+                case MembershipCreateStatus.DuplicateUserName:
+                    message += " Kullanılmış kullanıcı adı. ";
+                    break;
+                case MembershipCreateStatus.DuplicateEmail:
+                    message += " Kullanılmış mail adresi girildi. ";
+                    break;
+                case MembershipCreateStatus.UserRejected:
+                    message += " Kullanıcı Engel Hatası ";
+                    break;
+                case MembershipCreateStatus.InvalidProviderUserKey:
+                    message += " Geçersiz Kullanıcı Key Hatası ";
+                    break;
+                case MembershipCreateStatus.DuplicateProviderUserKey:
+                    message += " Kullanılmış kullanıcı key hatası. ";
+                    break;
+                case MembershipCreateStatus.ProviderError:
+                    message += " Üye Yönetimi Sağlayıcısı Hatası ";
+                    break;
+                default:
+                    break;
+            }
+
+            ViewBag.Message = message;
+
+            if (durum == MembershipCreateStatus.Success)
+                return RedirectToAction("Index");
+            else
+                return View();
+        }
+
+        public ActionResult RolAta()
+        {
+            ViewBag.Roller = Roles.GetAllRoles().ToList();
+            ViewBag.Kullanicilar = Membership.GetAllUsers();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RolAta(string KullaniciAdi, string RolAdi)
+        {
+            Roles.AddUserToRole(KullaniciAdi, RolAdi);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public string UyeRolleri(string kullaniciAdi)
+        {
+            List<string> roller = Roles.GetRolesForUser(kullaniciAdi).ToList();
+            string rol = "";
+            foreach (string r in roller)
+            {
+                rol += r + "-";
+            }
+            rol = rol.Remove(rol.Length - 1, 1);
+            return rol;
         }
     }
 }
---
+++ AspNetMVC_Template/Controllers/RoleController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AspNetMVC_Template.Controllers
+{
+    public class RoleController : Controller
+    {
+        // GET: Role
+        public ActionResult Index()
+        {
+            List<string> roller = Roles.GetAllRoles().ToList();
+            return View(roller);
+        }
+
+        [HttpGet]
+        public ActionResult RolEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RolEkle(string RolAdi)
+        {
+            Roles.CreateRole(RolAdi);
+            return RedirectToAction("Index");
+        }
+    }
+}
